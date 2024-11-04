e3446ece2895a1ecd51caaec7008111685ae91d8
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-11-04 14:48:47 +0800

    Added config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BACKEND_URL = os.getenv('BACKEND_URL')
+BACKEND_USERNAME = os.getenv('BACKEND_USERNAME')
+BACKEND_PASSWORD = os.getenv('BACKEND_PASSWORD')                                 
