93b164bc4291e55e2b4f39a4a6c93fce6d7c0b8b
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-11-06 17:27:50 +0800

    Added secret key for cookies

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from .routes import main as main_blueprint
+from config import SECRET_KEY
 
 def create_app():
     app = Flask(__name__)
 
+    app.secret_key = SECRET_KEY
+    
     app.register_blueprint(main_blueprint)
 
     return app
