e0f05cea8bdbb3bd4ce0899c951c6d052af01e1a
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-11-06 17:34:00 +0800

    Added SECRET_KEY var

--- config.py
+++ config.py
@@ -5,4 +5,6 @@ load_dotenv()
 
 BACKEND_URL = os.getenv('BACKEND_URL')
 BACKEND_USERNAME = os.getenv('BACKEND_USERNAME')
-BACKEND_PASSWORD = os.getenv('BACKEND_PASSWORD')                                 
+BACKEND_PASSWORD = os.getenv('BACKEND_PASSWORD')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
