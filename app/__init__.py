1c6394e6c88e8b1487ffbcb87d42fe6e71a247be
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-11-22 07:50:09 +0800

    Added timestamp conversion filter

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask
 from .routes import main as main_blueprint
 from config import SECRET_KEY
@@ -6,6 +7,14 @@ def create_app():
     app = Flask(__name__)
 
     app.secret_key = SECRET_KEY
+    
+    # Convert time from timestamp to DD:MM:YY HH:SS
+    @app.template_filter('format_datetime')
+    def format_datetime(value):
+        if value:
+            dt = datetime.datetime.fromtimestamp(int(value))
+            return dt.strftime('%d.%m.%Y %H:%M')
+        return ''
     
     app.register_blueprint(main_blueprint)
 
