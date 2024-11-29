73bbc050783673938b9169bc962b9f2702ee8ffb
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-11-29 16:01:58 +0800

    Fixed unprocessable entity when creating a secret

--- app/api/secrets.py
+++ app/api/secrets.py
@@ -1,19 +1,26 @@
+import json
 from app.api.requests import send_get_request, send_post_request, send_delete_request
 from config import BACKEND_URL
 
 
-async def create_secret(name: str, data, description: str, token: str, ttl: int = 0):
+async def create_secret(name: str, data, description: str, token: str, ttl: int = 3600):
     payload = {
         "name": name,
-        "data": {
-            data
-        },
+        "data": data,
         "tags": ["general"],
         "ttl": ttl,
         "description": description
     }
+    # Parse the 'data' field if it's a string
+    if isinstance(payload.get('data'), str):
+        try:
+            payload['data'] = json.loads(payload['data'])
+        except json.JSONDecodeError:
+            print("Error: 'data' field is not valid JSON.")
 
-    response =  await send_post_request("v1/secrets/create", payload, True, "json", token)
+    # Output the corrected payload
+    print(payload)
+    response = await send_post_request("v1/secrets/create", payload, True, "json", token)
     
     return response or None
 
