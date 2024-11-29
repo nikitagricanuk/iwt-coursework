085264f4c09a4b96a1efb3f1b5c564520e265258
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-11-29 12:28:59 +0800

    Added get_secret function

--- app/api/secrets.py
+++ app/api/secrets.py
@@ -1,6 +1,4 @@
-from json import loads
-
-from app.api.requests import send_get_request, send_post_request
+from app.api.requests import send_get_request, send_post_request, send_delete_request
 from config import BACKEND_URL
 
 
@@ -17,7 +15,13 @@ async def create_secret(name: str, data, description: str, token: str, ttl: int 
 
     response =  await send_post_request("v1/secrets/create", payload, True, "json", token)
     
-    return loads(response) if response else None
+    return response or None
+
+async def delete_secret(id: str, token: str):
+    return await send_delete_request(f"v1/secrets/{id}", token)
 
 async def list_all_secrets(token: str):
     return await send_get_request("v1/secrets", token)
+
+async def get_secret(id: str, token: str):
+    return await send_get_request(f"v1/secrets/{id}", token)
