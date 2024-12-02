23750634b53d56991fa392a98e1b9d027d9aff3a
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-12-02 12:47:26 +0800

    Only use cache if USE_RESIS is set

--- app/extensions.py
+++ app/extensions.py
@@ -1,17 +1,21 @@
+from functools import wraps
 from flask_caching import Cache
 from flask_compress import Compress
-from config import REDIS_HOST, REDIS_PORT
+from config import REDIS_HOST, REDIS_PORT, USE_REDIS
 
 cache = Cache()
 compress = Compress()
 
 def init_extensions(app):
-    # Cache configuration
-    app.config['CACHE_TYPE'] = 'redis'
-    app.config['CACHE_REDIS_HOST'] = REDIS_HOST
-    app.config['CACHE_REDIS_PORT'] = REDIS_PORT
-    app.config['CACHE_DEFAULT_TIMEOUT'] = 300
-
-    # Initialize extensions
+    if USE_REDIS:
+        # Cache configuration
+        app.config['CACHE_TYPE'] = 'redis'
+        app.config['CACHE_REDIS_HOST'] = REDIS_HOST
+        app.config['CACHE_REDIS_PORT'] = REDIS_PORT
+        app.config['CACHE_DEFAULT_TIMEOUT'] = 300
+    else:
+        app.config['CACHE_TYPE'] = 'NullCache'
+    
     cache.init_app(app)
+    
     compress.init_app(app)
